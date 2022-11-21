53a2062157653cb644f44f4fff7c19d556d0201a
MrMPerial <christopher.mitchell26@gmail.com>
2022-11-21 15:47:33 -0600

    added cloudinary config

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'mperial-web-solutions'
+  config.api_key = '938561814326735'
+  config.api_secret = 'QPUJetWRNBfDEjfIcZUnXCft6vM'
+  config.secure = true
+end
