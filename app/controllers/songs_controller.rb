f81034231615be7824dff0bd97220bd126dba8fb
MrMPerial <christopher.mitchell26@gmail.com>
2022-11-23 16:00:16 -0600

    added song model and controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,42 @@
 class SongsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
+  # Page
   def new
-    @profile = User.find( params[:user_id] ).profile
+    @user = User.find( params[:user_id] )
     @song = Song.new
   end
 
+  # Action
   def create
+    @user = User.find( params[:user_id] )
+    @song = @song.build_song(song_params)
+
+    if @song.save
+      flash[:success] = "Song Updated"
+      redirect_to user_path( params[:user_id] )
+    else
+      flash[:warning] = "Something went wrong."
+      render action: :new
+    end
   end
 
+  # Page
   def edit
   end
 
+  # Action
   def update
   end
+
+  private
+    def song_params
+      params.require(:song).permit(:title, :cover, :file)
+    end
+
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to root_url unless @user == current_user
+    end
 end
